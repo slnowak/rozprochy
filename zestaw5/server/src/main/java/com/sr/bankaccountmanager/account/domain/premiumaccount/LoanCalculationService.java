01fbfd1575c9e09e0e77b6cf42449fd0236591e5
novy154 <slnowak154@gmail.com>
2015-05-17 21:09:26 +0200

    Added loan policy (needs to be fixed)

--- zestaw5/server/src/main/java/com/sr/bankaccountmanager/account/domain/premiumaccount/LoanCalculationService.java
+++ zestaw5/server/src/main/java/com/sr/bankaccountmanager/account/domain/premiumaccount/LoanCalculationService.java
@@ -1,5 +1,6 @@
 package com.sr.bankaccountmanager.account.domain.premiumaccount;
 
+import FinancialNews.Currency;
 import com.sr.bankaccountmanager.news.domain.ExchangeRateRepository;
 import com.sr.bankaccountmanager.news.domain.ExchangeSides;
 import com.sr.bankaccountmanager.news.domain.InterestRepository;
@@ -18,12 +19,35 @@ public class LoanCalculationService {
     }
 
     public LoanDetails calculateLoan(LoanCalculationData calculationData) {
+//        todo: fix
         final ExchangeSides exchangeSides = calculationData.getExchangeSides();
-        final Float interestRate = interestRepository.interestRateFor(exchangeSides.to());
+        final float exchangedMoney = exchange(exchangeSides, calculationData.getAmount());
+        final float periodInterestRate = periodInterestRate(calculationData.getPeriod(), exchangeSides.to());
+        final float totalCostInForeignCurrency = totalCost(exchangedMoney, periodInterestRate);
+        final float totalCostInNationalCurrency = exchange(
+                ExchangeSides.of(exchangeSides.to(), exchangeSides.from()), totalCostInForeignCurrency
+        );
 
+        return new LoanDetails(
+                interestRepository.interestRateFor(exchangeSides.to()),
+                (int) totalCostInNationalCurrency
+        );
+
+    }
+
+    private float exchange(ExchangeSides exchangeSides, float amount) {
         final Float exchangeRate = exchangeRateRepository.exchangeRateFor(exchangeSides);
-        // todo: implement some logic
-        return new LoanDetails(interestRate, (int)(10000 + exchangeRate * 1000));
+        System.out.println(exchangeRate + " " + amount / exchangeRate);
+        return amount / exchangeRate;
+    }
 
+    private float periodInterestRate(int period, Currency currency) {
+        final float rate = interestRepository.interestRateFor(currency);
+        return (period / 12) * rate;
+    }
+
+
+    private float totalCost(float exchangedAmount, float periodInterestRate) {
+        return exchangedAmount + exchangedAmount * periodInterestRate;
     }
 }
